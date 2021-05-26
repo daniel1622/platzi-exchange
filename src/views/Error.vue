31b9edb1175a969f6840b75a42e2c2875e7fc865
Daniel Pardo <danielpa06@gmail.com>
2021-05-26 17:09:10 -0500

    Project kick off 🐲

---
+++ src/views/Error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl uppercase">error</h1>
+    <p class="p-5">Esta página no está disponible</p>
+    <p class="text-sm">
+      Es posible que el enlace que seleccionaste esté dañado o que se haya
+      eliminado la página.
+    </p>
+    <p class="text-9xl p-5">🐱‍🏍</p>
+    <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/">
+      Volver a la página de Inicio
+    </router-link>
+  </div>
+</template>
